c1d4dbda783f8136899a7cb5519702bb015e482b
ktk3376 <ktk3376@gmail.com>
2026-03-25 16:48:05 +0900

    merge: feature/detailed 브랜치 병합 전 준비2

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = (import.meta.env.VITE_SUPABASE_URL = 'https://yrvvkxhrqjuwfjvkkxbl.supabase.co')
-const supabaseAnonKey = (import.meta.env.VITE_SUPABASE_ANON_KEY = 'sb_publishable_grpYAXDjeprZ9niKlh8l7A_Q9MvAabx')
+const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
