5b7b5eb4c8fd3ece73e784f28382c25ff588abe5
yunabin <yhb1020525@gmail.com>
2026-03-26 12:10:43 +0900

    ui: 자유 게시판 UI

--- src/app/components/Root.tsx
+++ src/app/components/Root.tsx
@@ -1,3 +1,4 @@
+import ModalProvider from '@/provider/modal-provider'
 import { Outlet } from 'react-router'
 import { AuthProvider } from '../contexts/AuthContext'
 import { Header } from './Header'
@@ -5,12 +6,14 @@ import { Header } from './Header'
 export function Root() {
     return (
         <AuthProvider>
-            <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-                <Header />
-                <main className="container mx-auto px-4 py-8">
-                    <Outlet />
-                </main>
-            </div>
+            <ModalProvider>
+                <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+                    <Header />
+                    <main className="container mx-auto px-4 py-8">
+                        <Outlet />
+                    </main>
+                </div>
+            </ModalProvider>
         </AuthProvider>
     )
 }
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,31 +1,31 @@
-import { createBrowserRouter } from "react-router";
-import { Root } from "./components/Root";
-import { Home } from "./components/Home";
-import { FacilityDetail } from "./components/FacilityDetail";
-import { LoginWrapper as Login } from "./components/LoginWrapper";
-import { RegisterWrapper as Register } from "./components/RegisterWrapper";
-import { MyPage } from "./components/MyPage";
-import { Community } from "./components/Community";
-import { NotFound } from "./components/NotFound";
+import { createBrowserRouter } from 'react-router'
+import { Community } from './components/Community'
+import { FacilityDetail } from './components/FacilityDetail'
+import { Home } from './components/Home'
+import { LoginWrapper as Login } from './components/LoginWrapper'
+import { MyPage } from './components/MyPage'
+import { NotFound } from './components/NotFound'
+import { RegisterWrapper as Register } from './components/RegisterWrapper'
+import { Root } from './components/Root'
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Root,
-    children: [
-      { index: true, Component: Home },
-      { path: "facility/:id", Component: FacilityDetail },
-      { path: "mypage", Component: MyPage },
-      { path: "community", Component: Community },
-      { path: "*", Component: NotFound },
-    ],
-  },
-  {
-    path: "/login",
-    Component: Login,
-  },
-  {
-    path: "/register",
-    Component: Register,
-  },
-]);
+    {
+        path: '/',
+        Component: Root,
+        children: [
+            { index: true, Component: Home },
+            { path: 'facility/:id', Component: FacilityDetail },
+            { path: 'mypage', Component: MyPage },
+            { path: 'community', Component: Community },
+            { path: '*', Component: NotFound },
+        ],
+    },
+    {
+        path: '/login',
+        Component: Login,
+    },
+    {
+        path: '/register',
+        Component: Register,
+    },
+])
